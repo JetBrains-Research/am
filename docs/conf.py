e10d69004ebea0b28f14fde4b9b4fc3ef52d624d
Farid.Bagirov <farid.bagirov@jetbrains.com>
2023-03-24 15:48:18 +0200

    update files for the docs

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath(''))
 sys.path.insert(0, os.path.abspath('docs'))
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'src')))
+sys.path.insert(0, os.path.abspath(os.path.join('', '..', 'src')))
 # -- Project information
 
 project = 'am'
