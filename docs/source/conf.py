22a5ff15a9f6a62228070d096e1669a3d5e60e97
Farid.Bagirov <farid.bagirov@jetbrains.com>
2023-03-24 15:34:00 +0200

    update files for the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,9 +3,8 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(''))
-sys.path.insert(0, os.path.abspath('am'))
-sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('docs'))
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'src')))
 # -- Project information
 
 project = 'am'
